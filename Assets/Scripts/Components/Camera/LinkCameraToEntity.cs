d383ef4204eec90bee978ea11c7630f28c915831
Jon Edwards <jon@laguna.games>
2024-09-08 10:32:27 -0700

    Spawns squire

---
+++ Assets/Scripts/Components/Camera/LinkCameraToEntity.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class LinkCameraToEntity : MonoBehaviour
+{
+    public PlaneCameraController cameraController;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/Systems/SpawnerSystem.cs
@@ -0,0 +1,42 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace HelloCube.Prefabs
+{
+    public partial struct SpawnSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            // This call makes the system not update unless at least one entity in the world exists that has the Spawner component.
+            state.RequireForUpdate<Spawner>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            // Create a query that matches all entities having a RotationSpeed component.
+            // (The query is cached in source generation, so this does not incur a cost of recreating it every update.)
+            var squireQuery = SystemAPI.QueryBuilder().WithAll<SquireTag>().Build();
+
+            // Only spawn cubes when no cubes currently exist.
+            if (squireQuery.IsEmpty)
+            {
+                var prefab = SystemAPI.GetSingleton<Spawner>().Prefab;
+
+                // Instantiating an entity creates copy entities with the same component types and values.
+                var instances = state.EntityManager.Instantiate(prefab, 1, Allocator.Temp);
+
+                foreach (var entity in instances)
+                {
+                    // Position the squire
+                    var transform = SystemAPI.GetComponentRW<LocalTransform>(entity);
+                    transform.ValueRW.Position = new float3(0, 0, 0);
+                }
+            }
+        }
+    }
+}
