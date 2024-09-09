5b034c9460d7368d7799f406aae562240ea4dabe
Jon Edwards <jon@laguna.games>
2024-09-09 11:48:29 -0700

    Settings move destination but not working

---
+++ Assets/Scripts/Systems/MoveRequestInitSystem.cs
@@ -0,0 +1,32 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[InternalBufferCapacity(8)] // Adjust based on expected usage
+public struct MoveRequestBufferElement : IBufferElementData
+{
+    public float3 TargetPosition;
+}
+
+public partial class MoveRequestInitSystem : SystemBase
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        // We need to wait for the scene to load before Updating, so we must RequireForUpdate at
+        // least one component type loaded from the scene.
+        state.RequireForUpdate<ExecuteDungeonSync>();
+    }
+
+    protected override void OnUpdate()
+    {
+        Enabled = false;
+        
+        // Check if the singleton already exists
+        if (!SystemAPI.HasSingleton<MoveRequestBufferElement>())
+        {
+            var singletonEntity = EntityManager.CreateEntity();
+            EntityManager.AddBuffer<MoveRequestBufferElement>(singletonEntity);
+        }
+    }
+}
