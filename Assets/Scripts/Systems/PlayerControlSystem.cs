209b67b2aea6256a1cd910aaea7cf10cfd5d0fa7
Jon Edwards <heroiclogic@gmail.com>
2024-09-23 16:45:48 -0700

    PlayerControlSystem is detecting ground hit

--- Assets/Scripts/Systems/PlayerControlSystem.cs
+++ Assets/Scripts/Systems/PlayerControlSystem.cs
@@ -1,4 +1,5 @@
 using Unity.Entities;
+using UnityEngine;
 
 [UpdateInGroup(typeof(SimulationSystemGroup))]
 public partial struct PlayerControlSystem : ISystem
@@ -6,6 +7,7 @@ public partial struct PlayerControlSystem : ISystem
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<PlayerTag>();
+        state.RequireForUpdate<SquireTag>();
     }
 
     public void OnDestroy(ref SystemState state)
@@ -16,10 +18,18 @@ public partial struct PlayerControlSystem : ISystem
     {
         // Check for has TargetPosition component
         var playerEntity = SystemAPI.GetSingletonEntity<PlayerTag>();
-        if (SystemAPI.HasComponent<TargetPosition>(playerEntity))
+        if (SystemAPI.IsComponentEnabled<TargetPosition>(playerEntity))
         {
             var targetPosition = SystemAPI.GetComponent<TargetPosition>(playerEntity);
+            var squireEntity = SystemAPI.GetSingletonEntity<SquireTag>();
+            var squirePendingActions = SystemAPI.GetComponent<PendingAgentActions>(squireEntity);
+            
+             Debug.LogFormat("PlayerControlSystem: found target position {0}", targetPosition.targetPosition);
+
             // TODONOW Create a MoveToPosition action entity and add it to action queue for squire
+            
+            state.EntityManager.SetComponentEnabled<TargetPosition>(playerEntity, false);
+            state.EntityManager.SetComponentEnabled<TargetEntity>(playerEntity, false);
         }
     }
 }
