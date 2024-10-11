30be369c8ea69de0f19ff791ee1de0545b84d9a5
Jon Edwards <heroiclogic@gmail.com>
2024-10-11 12:01:53 -0700

    Utility AI selecting between idle and return to squire

--- Assets/Scripts/Authoring/HeroAuthoring.cs
+++ Assets/Scripts/Authoring/HeroAuthoring.cs
@@ -6,6 +6,9 @@ public class HeroAuthoring : MonoBehaviour
 {
     public HeroConsiderationSetData ConsiderationSetData;
 
+    public float DecisionInertia = 0.5f;
+    public float MaxDistanceFromSquire = 100.0f;
+
     public class HeroBaker : Baker<HeroAuthoring>
     {
         public override void Bake(HeroAuthoring authoring)
@@ -18,18 +21,27 @@ public class HeroAuthoring : MonoBehaviour
             AddComponent(entity, new AgentActiveActionType());
 
             HeroAI heroAI = new HeroAI();
+            heroAI.DecisionInertia = authoring.DecisionInertia;
+            heroAI.ShouldUpdateReasoner = true;
+            heroAI.TimeSinceMadeDecision = float.MaxValue;
+            heroAI.MaxDistanceFromSquire = authoring.MaxDistanceFromSquire;
+
             authoring.ConsiderationSetData.Bake(this, out HeroConsiderationSet considerationSetComponent);
             ReasonerUtilities.BeginBakeReasoner(this, out Reasoner reasoner, out DynamicBuffer<Action> actionsBuffer,
                 out DynamicBuffer<Consideration> considerationsBuffer,
                 out DynamicBuffer<ConsiderationInput> considerationInputsBuffer);
             {
-                ReasonerUtilities.AddAction(new ActionDefinition((int)HeroAIAction.Idle), true, ref reasoner, actionsBuffer, out heroAI.IdleRef);
-                ReasonerUtilities.AddAction(new ActionDefinition((int)HeroAIAction.ReturnToSquire), true, ref reasoner, actionsBuffer, out heroAI.ReturnToSquireRef);
-                
-                ReasonerUtilities.AddConsideration(considerationSetComponent.IdlingComfort, ref heroAI.IdleRef, true, ref reasoner, actionsBuffer, considerationsBuffer, considerationInputsBuffer, out heroAI.IdlingComfortRef);
+                ReasonerUtilities.AddAction(new ActionDefinition((int)HeroAIAction.Idle), true, ref reasoner,
+                    actionsBuffer, out heroAI.IdleRef);
+                ReasonerUtilities.AddAction(new ActionDefinition((int)HeroAIAction.ReturnToSquire), true, ref reasoner,
+                    actionsBuffer, out heroAI.ReturnToSquireRef);
+
+                ReasonerUtilities.AddConsideration(considerationSetComponent.IdlingComfort, ref heroAI.IdleRef, true,
+                    ref reasoner, actionsBuffer, considerationsBuffer, considerationInputsBuffer,
+                    out heroAI.IdlingComfortRef);
                 ReasonerUtilities.AddConsideration(considerationSetComponent.DistanceFromSquire,
                     ref heroAI.ReturnToSquireRef, true, ref reasoner, actionsBuffer, considerationsBuffer,
-                    considerationInputsBuffer, out heroAI.IdlingComfortRef);
+                    considerationInputsBuffer, out heroAI.DistanceFromSquireRef);
             }
             ReasonerUtilities.EndBakeReasoner(this, reasoner);
 
